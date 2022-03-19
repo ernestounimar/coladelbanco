c20e488392ea7046d6e7cdba35494618e1b9eea5
ernestounimar <93153130+ernestounimar@users.noreply.github.com>
2022-03-19 12:03:58 -0400

    cola con la clase archivo
    añadí la clase archivo a la cola que ya estaba hecha

--- cola/src/cola/Cola.java
+++ cola/src/cola/Cola.java
@@ -4,11 +4,43 @@
  */
 package cola;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Usuario
  */
-
+class archivo
+{
+    PrintWriter pf;
+    FileReader fr;
+    
+    void crear(String pNombre,String pDatos) throws IOException
+    {
+        pf=new PrintWriter(new FileWriter(pNombre, true));
+        pf.println(pDatos);
+        pf.close();
+        System.out.println("Generacion ok");
+        
+    }
+void leer(String pnombre,Cola lis) throws IOException{
+    FileReader fr=new FileReader(pnombre);
+        BufferedReader br=new BufferedReader(fr);
+        String linea=br.readLine();
+       
+        while ((linea=br.readLine())!=null)
+        {
+            String datos;
+            datos=("/r");
+            String nombre = datos;
+            lis.insertar(nombre);
+    }
+}
+}
 public class Cola {
 
     
