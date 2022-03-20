cb45eb4194ef293f574045a0841e30a5afa6d6d4
Adriancito449 <adriantesta00@gmail.com>
2022-03-20 19:55:14 -0400

    Arquitectura de lo basico

---
+++ cola/src/cola/Cliente.java
@@ -0,0 +1,26 @@
+package cola;
+
+/* Esta clase se usa para declarar los datos que se manejaran a travez de la cola 
+si se desea encanzular alguno pueden hacerlo en este apartado  */
+
+public class Cliente {
+    String Nombre; 
+    Integer Edad; 
+    Boolean Discapacidad; 
+    Integer Posicion;
+    
+    public Cliente(String nombre, Integer edad, Integer posicion) {
+        Nombre = nombre;
+        Edad = edad;
+        Posicion = posicion;
+    }
+
+    
+}
+
+
+
+
+
+/* Alfinal no se como van a porner el formato 
+fecha para las entradas de los usuarios pero aqui hiria  */
---
+++ cola/src/cola/Main.java
@@ -0,0 +1,22 @@
+package cola;
+
+public class Main {
+
+    /*
+     * aqui defino los punteros para
+     * los metodos con los que manipulo las colas
+     */
+
+
+    public static void main(String[] args) {
+
+
+        Cliente Rodolfo = new Cliente(jose,12,3); 
+
+        System.out.println(Cliente.class.Nombre); 
+
+        System.out.println(" ok ");
+
+    }
+
+}
