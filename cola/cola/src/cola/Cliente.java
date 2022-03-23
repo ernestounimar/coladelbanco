6078b7f86c8c93f52f63c7e55828ad42954a2ee6
PedroHernandez14 <93164330+PedroHernandez14@users.noreply.github.com>
2022-03-22 23:56:51 -0400

    cola con operaciones

---
+++ cola/cola/src/cola/Cliente.java
@@ -0,0 +1,30 @@
+package cola;
+
+import java.io.BufferedReader;
+import java.util.Scanner;
+
+/* Esta clase se usa para declarar los datos que se manejaran a travez de la cola 
+si se desea encanzular alguno pueden hacerlo en este apartado  */
+
+public class Cliente {
+    String Nombre;
+    Integer Edad;
+    Boolean Discapacidad;
+    Integer Posicion;
+    Integer Tiempo;
+    public Cliente(String nombre, Integer edad, Boolean discapacidad, Integer posicion,Integer tiempo) {
+        Nombre = nombre;
+        Edad = edad;
+        Discapacidad = discapacidad;
+        Posicion = posicion;
+        Cliente.operaciones
+        
+    }
+
+    
+}
+
+/*
+ * Alfinal no se como van a porner el formato
+ * fecha para las entradas de los usuarios pero aqui hiria
+ */
